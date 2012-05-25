3906a9f1a7b845d4f59a088146a82dd6a94db3ba
David Madrigal <mc.david.madrigal@gmail.com>
2012-05-25 14:30:45 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MagmarailsBlog::Application.config.secret_token = '26f8338a161e1e42af1cfd05054637318515d3a3320e325ca0642833a8a3d92f2deef1f1e09fa3539f74bc41af0283f924ee2a859198824ce5923304ba250a27'
