fcd46ab8b4c5400bd12a2d04064661217d026225
David Madrigal <mc.david.madrigal@gmail.com>
2012-05-26 16:41:40 -0500

    dynamic link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def link_for_user_login_or_logout
+    if current_user
+      link_to t('views.post.log_out'), destroy_user_session_path, :method => :delete 
+    else
+      link_to t('views.post.log_in'), new_user_session_path
+    end
+  end
 end
